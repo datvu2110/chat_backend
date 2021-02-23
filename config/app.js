272b222b1a5be07417ecd3fd36ef303f68c0eaee
Derek Vu <beetee@Dereks-MacBook-Pro.local>
2021-02-22 18:48:54 -0600

    Update the models folder

--- config/app.js
+++ config/app.js
@@ -3,5 +3,7 @@ require('dotenv').config()
 module.exports = {
     appKey: process.env.APP_KEY,
     appUrl: process.env.APP_URL,
-    appPort: process.env.APP_PORT
+    appPort: process.env.APP_PORT,
+    port: process.env.PORT || 8000
+
 }
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(router)
 app.use(express.static(__dirname + '/public'))
 app.use(express.static(__dirname + '/uploads'))
 
-const port = config.appPort
+const port = config.port
 
 const server = http.createServer(app)
 const SocketServer = require('./socket')
