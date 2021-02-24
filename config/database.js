b960a13b09e0647ea7591a4422f8c87875bc4ddf
Derek Vu <beetee@Dereks-MacBook-Pro.local>
2021-02-23 18:48:38 -0600

    update the database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 
+
 module.exports = {
     "development": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_DATABASE,
-        "host": process.env.DB_HOST,
+        "username": process.env.DB_USER_PROD,
+        "password": process.env.DB_PASSWORD_PROD,
+        "database": process.env.DB_DATABASE_PROD,
+        "host": process.env.DB_HOST_PROD,
         "dialect": "postgres",
         "logging": false
     },
