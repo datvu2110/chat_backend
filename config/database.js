90daba1940a7675ea06cde5a7316dbd15a676a02
Derek Vu <beetee@Dereks-MacBook-Pro.local>
2021-02-27 13:43:01 -0600

    Update the env

--- config/database.js
+++ config/database.js
@@ -1,12 +1,16 @@
 require('dotenv').config()
 
+let DB_HOST_PROD = "ec2-54-165-164-38.compute-1.amazonaws.com"
+let DB_USER_PROD = "pzosfuutpnktdp"
+let DB_PASSWORD_PROD = "afc22b2e86b508082ae81f434bc5fbfa698904c797767749c4c016ce35d9cedb"
+let DB_DATABASE_PROD = "d1au4drbo6gkl7"
 
 module.exports = {
     "development": {
-        "username": process.env.DB_USER_PROD,
-        "password": process.env.DB_PASSWORD_PROD,
-        "database": process.env.DB_DATABASE_PROD,
-        "host": process.env.DB_HOST_PROD,
+        "username": DB_USER_PROD,
+        "password": DB_PASSWORD_PROD,
+        "database": DB_DATABASE_PROD,
+        "host": DB_HOST_PROD,
         "dialect": "postgres",
         "logging": false
     },
@@ -19,10 +23,10 @@ module.exports = {
         "logging": false
     },
     "production": {
-        "username": process.env.DB_USER_PROD,
-        "password": process.env.DB_PASSWORD_PROD,
-        "database": process.env.DB_DATABASE_PROD,
-        "host": process.env.DB_HOST_PROD,
+        "username": DB_USER_PROD,
+        "password": DB_PASSWORD_PROD,
+        "database": DB_DATABASE_PROD,
+        "host": DB_HOST_PROD,
         "dialect": "postgres",
         "dialectOptions":{
             "ssl": true
